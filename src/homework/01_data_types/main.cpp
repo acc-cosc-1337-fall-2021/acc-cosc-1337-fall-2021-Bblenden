ca3ea3b4ca590158d3c4243e65e1c02c44cae805
Bblenden <christian.blenden@g.austincc.edu>
2021-09-08 18:23:24 -0500

    hw 1 w/ test cases

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,4 +1,6 @@
 ﻿//write include statements
+#include<iostream>
+#include"data_types.h"
 
 
 //write namespace using statement for cout
@@ -9,7 +11,13 @@ Call multiply_numbers with 10 and 10 parameter values and display function resul
 */
 int main()
 {
-	
-
+	int num;
+	std::cin >> num;
+	int result;
+	result = multiply_numbers(num);
+	std::cout << result << '\n';
+	int num1 = 4;
+	result = multiply_numbers(num1);
+	std::cout << result << '\n';
 	return 0;
 }
