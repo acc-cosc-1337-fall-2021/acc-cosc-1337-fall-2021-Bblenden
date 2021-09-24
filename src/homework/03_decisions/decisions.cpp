a0a8662cdd53bd68aa7ccb6c750207442907dc1f
Bblenden <christian.blenden@g.austincc.edu>
2021-09-23 20:06:32 -0500

    hw 3 w/ test cases

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,60 @@
-//write include statement for decisions header
+#include<iostream>
+#include<string>
+#include<decisions.h>
 
 
-//Write code for function(s) code here
+using namespace std;
+char letterGrade;
+
+
+char get_letter_grade_using_if(int grade)
+{
+    if (grade >= 90)
+    {
+        letterGrade = 'A';
+    }
+
+    else if (grade <= 89 && grade >= 80)
+    {
+        letterGrade = 'B';
+    }
+
+    else if (grade <= 79 && grade >= 70)
+    {
+        letterGrade = 'C';
+    }
+
+    else if (grade <= 69 && grade >= 60)
+    {
+        letterGrade = 'D';
+    }
+
+    else if (grade <= 59)
+    {
+        letterGrade = 'F';
+    }
+    return letterGrade;
+}
+
+char get_letter_grade_using_switch(int grade)
+{
+    switch (grade)
+    {
+    case 90 ... 100:
+        letterGrade = 'A';
+        break;
+    case 80 ... 89:
+        letterGrade = 'B';
+        break;
+    case 70 ... 79:
+        letterGrade = 'C';
+        break;
+    case 60 ... 69:
+        letterGrade = 'D';
+        break;
+    case 0 ... 59:
+        letterGrade = 'F';
+        break;
+    }
+    return letterGrade;
+}
--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,51 @@
 //write include statements
+#include<iostream>
+#include"decisions.h"
+
+using namespace std;
 
 int main() 
 {
+	int decision;
+	int grade;
+	char letterGrade;
+	cout << "Welcome to the Letter Grade calculator!" << '\n';
+	cout << "MAIN MENU: (please enter the number for the menu option you would like.)" << '\n' << "1. Letter grade using if" << '\n' << "2. Letter grade using switch" << '\n' << "3. EXIT" << '\n';
+	cin >> decision;
+
+	switch (decision)
+	{
+		case 1:
+		cout << "|Letter Grade using if|" << '\n';
+		cout << "Enter the numeric grade value: ";
+		cin >> grade;
+		if (grade > 100 || grade < 0)
+		{
+			cout << '\n' << "That number is out of range";
+			break;
+		}
+		letterGrade = get_letter_grade_using_if(grade);
+		cout << '\n' << "The letter grade is: " << letterGrade;
+		break;
+
+		case 2:
+		cout << "|Letter Grade using switch|" << '\n';
+		cout << "Enter the numeric grade value: ";
+		cin >> grade;
+		if (grade > 100 || grade < 0)
+		{
+			cout << '\n' << "That number is out of range";
+			break;
+		}
+		letterGrade = get_letter_grade_using_switch(grade);
+		cout << '\n' << "The letter grade is: " << letterGrade;
+		break;
+
+		case 3:
+		cout << "Exiting Program...";
+
+		default:
+		break;
+	}
 	return 0;
 }
