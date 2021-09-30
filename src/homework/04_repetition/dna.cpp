64167c481c88c2061f1134b52103181b2c576fad
Bblenden <christian.blenden@g.austincc.edu>
2021-09-30 17:06:35 -0500

    hw 4 w/ test cases

--- src/homework/04_repetition/dna.cpp
+++ src/homework/04_repetition/dna.cpp
@@ -1,3 +1,33 @@
 //add include statements
-
+#include<iostream>
+#include "dna.h"
 //add function(s) code here
+
+int factorial (int num)
+{
+    int total = 1;
+    while (num > 0)
+    {
+        total = total * num;
+        num--;
+    }
+    return total;
+}
+
+int gcd (int num1, int num2)
+{
+    while (num1 != num2)
+    {
+        if (num1 < num2)
+        {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+        else if (num1 > num2)
+        {
+           num1 -= num2; 
+        }
+    }
+    return num1;
+}
