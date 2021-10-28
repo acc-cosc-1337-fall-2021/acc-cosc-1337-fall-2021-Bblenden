b2ff947361a8918ec4c0ced4589469d4a0c9d61e
Bblenden <christian.blenden@g.austincc.edu>
2021-10-28 17:42:48 -0500

    hw 6 week 2 /w test cases

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -16,8 +16,28 @@ int main()
 	bool game_over;
 
 	cout<<"Welcome to Tic Tac Toe! \n-----------------------------\n\n";
-	cout<<"Would you like to be Xs or Os? \n";
-	cin>>first_player;
+
+	int correct_choice = 0;
+	do
+	{
+		cout<<"Would you like to be Xs or Os? \n";
+		cin>>first_player;
+
+		if (first_player == "X" || first_player == "O")
+		{
+			correct_choice = 1;
+
+		}
+		else
+		{
+			correct_choice = 0;
+		}
+
+	} while (correct_choice == 0);
+	
+
+	
+
 	do
 	{
 		game.start_game(first_player);
@@ -30,6 +50,15 @@ int main()
 			game.display_board();
 			game_over = game.game_over();
 		}while(game_over == false);
+
+		if (game.get_winner() == "X" || game.get_winner() == "O")
+		{
+			cout<<"\nThe winner is "<<game.get_winner();
+		}
+		else if (game.get_winner() == "C")
+		{
+			cout<<"\nLooks like the game is a tie!";
+		}
 		cout<<"\nWould you like to play again? (Y/N): \n";
 		cin>>player_choice;
 	
--- src/homework/06_tic_tac_toe/tic_tac_toe.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe.h
@@ -18,12 +18,18 @@ public:
     string get_player()const;
     // displays the board
     void display_board()const;
+    string get_winner();
 
 private:
     void set_next_player();
     bool check_board_full();
     void clear_board();
+    bool check_column_win();
+    bool check_row_win();
+    bool check_diagonal_win();
+    void set_winner();
     string player;
     vector<string> pegs = vector<string>(9," ");
+    string winner;
 
 };
